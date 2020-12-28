a31f9f7b3416064c66972cbcef804e1310eb6fed
thewongwayround <70138507+thewongwayround@users.noreply.github.com>
2020-12-28 09:44:05 +0800

    sponsor page new fintechlab
    added fintech lab to sponsor page

--- fintech-month/src/App.js
+++ fintech-month/src/App.js
@@ -17,6 +17,7 @@ import Stanley from './pages/sponsors/Stanley';
 import Genping from './pages/sponsors/Genping';
 import EventOverviewPage from './pages/home/EventOverviewPage';
 import RegDatePage from './pages/home/RegDatePage'
+import FinTechLab from './pages/sponsors/FintechLab.js';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -48,7 +49,7 @@ function App() {
           </div>
           <div className={classes.emptyitem}></div>
           <Button color="inherit" href="/" className={classes.title}>Home</Button>
-          <Button color="inherit" href="/sponsors/fundedhere" className={classes.title}>Sponsors</Button>
+          <Button color="inherit" href="/sponsors/fintechlab" className={classes.title}>Sponsors</Button>
           <Button color="inherit" href="/faq" className={classes.title}>FAQ</Button>
           <Button color="inherit" href="/blog" className={classes.title}>Blog</Button>
           <div className={classes.emptyitem}></div>
@@ -60,7 +61,8 @@ function App() {
 
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/hackathon" exact component={HackathonPage} />        <Route path="/sponsors/fundedhere" exact component={Fundedhere} />
+        <Route path="/hackathon" exact component={HackathonPage} />
+        <Route path="/sponsors/fintechlab" exact component={FinTechLab} />        <Route path="/sponsors/fundedhere" exact component={Fundedhere} />
         <Route path="/sponsors/bnpp" exact component={BNPP} />
         <Route path="/sponsors/stanley" exact component={Stanley} />
         <Route path="/sponsors/genping" exact component={Genping} />

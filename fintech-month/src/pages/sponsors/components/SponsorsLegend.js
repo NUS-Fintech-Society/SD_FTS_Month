f523a7ef5dfd0216e8280528c97b6dffe4451612
thewongwayround <70138507+thewongwayround@users.noreply.github.com>
2021-01-08 15:00:27 +0800

    blog page and partners page update
    fixed blog page button links and commented out the navbar button and  app routing

--- fintech-month/src/pages/sponsors/components/SponsorsLegend.js
+++ fintech-month/src/pages/sponsors/components/SponsorsLegend.js
@@ -4,16 +4,16 @@ import '../SponsorsPage.css';
 function SponsorsLegend() {
     return (
         <div class='sidenav'>
-            <h1>Our Sponsors</h1>
-            <a href='/sponsors/fintechlab'>FinTech Lab</a>
+            <h1>Our Partners</h1>
+            <a href='/partners/fintechlab'>FinTech Lab</a>
             <hr />
-            <a href='/sponsors/fundedhere'>FundedHere</a>
+            <a href='/partners/fundedhere'>FundedHere</a>
             <hr />
-            <a href='/sponsors/bnpp'>BNPP</a>
+            <a href='/partners/bnpp'>BNPP</a>
             <hr />
-            <a href='/sponsors/stanley'>Prof Stanley</a>
+            <a href='/partners/stanley'>Prof Stanley</a>
             <hr />
-            <a href='/sponsors/genping'>Mr Liu Genping</a>
+            <a href='/partners/genping'>Mr Liu Genping</a>
             <hr />
         </div>
     );

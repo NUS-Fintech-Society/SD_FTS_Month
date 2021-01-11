e41e1fabe6611f9a165cdf6e4f756514cb1f6f6c
Chesterwongz <70138507+Chesterwongz@users.noreply.github.com>
2021-01-11 23:14:53 +0800

    added mr edwin to bnpp

--- fintech-month/src/pages/sponsors/BNPP.js
+++ fintech-month/src/pages/sponsors/BNPP.js
@@ -2,37 +2,48 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import SideNav from './components/SponsorsLegend';
 import WaiKokPhoto from '../../images/waikokphoto.jpg';
+import EdiwinChanPhoto from '../../images/edwinchanphoto.png';
 import './SponsorsPage.css';
 
 function FundedHere() {
-    const link = 
-        <a 
-        target="_blank" 
-        rel='noreferrer noopener' 
-        href='https://www.bnpparibas.com.sg/en/'>
+    const link =
+        <a
+            target="_blank"
+            rel='noreferrer noopener'
+            href='https://www.bnpparibas.com.sg/en/'>
             BNP Paribas
         </a>
     return (
         <>
             <SideNav />
-            <Grid class='main' justify='center'>
+            <Grid className='main-bnpp' justify='center'>
                 <h1>
                     <a target="_blank" rel='noreferrer noopener' href='https://www.linkedin.com/in/kok-wai-hon-3269632b/'>
                         Mr Hon Kok Wai
                     </a>
                 </h1>
-                <img class='profile' src={WaiKokPhoto} alt='WaiKok' />
+                <img src={WaiKokPhoto} alt='WaiKok' />
                 <h5 class='desc'>
-                    Runs market management, pricing and billing functions 
-                    for the APAC region within {link} Transaction Banking. 
-                    Responsibilities include assessment and development of onshore capabilities 
-                    through platform up-tiering, bank partnership; 
-                    business development in supporting client conversations 
-                    and propositions in pitching to treasury hubs in APAC. 
-                    His current professional interests are Foreign Exchange Payments, 
-                    Mobile Money and Alternate payments. 
-                    He also specialises in Payments and Collections Product Management, 
+                    Runs market management, pricing and billing functions
+                    for the APAC region within {link} Transaction Banking.
+                    Responsibilities include assessment and development of onshore capabilities
+                    through platform up-tiering, bank partnership;
+                    business development in supporting client conversations
+                    and propositions in pitching to treasury hubs in APAC.
+                    His current professional interests are Foreign Exchange Payments,
+                    Mobile Money and Alternate payments.
+                    He also specialises in Payments and Collections Product Management,
                     and Process Improvement and Revenue Optimization.
+                </h5>
+                
+                <h1>
+                    <a target="_blank" rel='noreferrer noopener' href='https://www.linkedin.com/in/edwin-chan-89806012/'>
+                        Mr Edwin Chan
+                    </a>
+                </h1>
+                <img src={EdiwinChanPhoto} alt='EdwinChan' />
+                <h5 class='desc'>
+                    Head, Product and Market Management, Transaction Banking APAC
                 </h5>
             </Grid>
         </>

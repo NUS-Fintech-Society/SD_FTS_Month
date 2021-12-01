7074409474139050034ae01408afd4be3d128414
jianoway <77208762+jianoway@users.noreply.github.com>
2021-12-01 21:53:54 +0800

    Add rest of sponsors

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Box, Button, Typography } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
+
 const useStyles = makeStyles((theme) => ({
   root: {
+    position: 'relative',
     height: '100vh',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    // backgroundColor: theme.palette.background.secondary,
+    backgroundColor: theme.palette.background.secondary,
     flexDirection: 'column',
-    position: 'relative',
-    backgroundColor: '#123123',
   },
   sectionTitle: {
     position: 'absolute',
@@ -35,35 +35,72 @@ const useStyles = makeStyles((theme) => ({
     height: '40px',
     textAlign: 'center',
   },
+  partnersTitle: {
+    position: 'absolute',
+    top: '63%',
+    width: '953px',
+    height: '40px',
+    textAlign: 'center',
+  },
   sponsorLogoWrapper: {
     position: 'absolute',
     top: '37%',
+    display: 'flex',
     height: '240px',
+    width: '1074px',
     alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'center',
   },
-
   header: {
     fontWeight: 700,
   },
   bodyWrapper: {
     marginTop: 16,
     textAlign: 'center',
+    justifyContent: 'center',
   },
-  iconWrapper: {
-    marginTop: 16,
-    '& :not(:first-child)': {
-      marginLeft: 12,
-    },
-    '& .MuiSvgIcon-root': {
-      color: theme.palette.secondary.main,
-      fontSize: 32,
-    },
+  bybitLogoButton: {
+    border: '0px',
+    paddingLeft: '24px',
+    paddingRight: '24px',
+    backgroundColor: theme.palette.background.secondary,
   },
-  buttonWrapper: {
-    marginTop: 16,
-    '& :not(:first-child)': {
-      marginLeft: 12,
-    },
+  rippleLogoButton: {
+    border: '0px',
+    paddingLeft: '24px',
+    paddingRight: '24px',
+    backgroundColor: theme.palette.background.secondary,
+    width: '40%',
+  },
+  stellarLogoButton: {
+    border: '0px',
+    paddingLeft: '24px',
+    paddingRight: '24px',
+    backgroundColor: theme.palette.background.secondary,
+  },
+  partnersLogoWrapper: {
+    position: 'absolute',
+    top: '66%',
+    display: 'flex',
+    height: '360px',
+    paddingTop: '20px',
+    width: '1000px',
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'center',
+    verticalAlign: 'center',
+  },
+  nusLogoButton: {
+    border: '0px',
+    backgroundColor: theme.palette.background.secondary,
+    width: '360px',
+  },
+  ftsLogo: {
+    backgroundColor: theme.palette.background.secondary,
+    width: '360px',
+    positon: 'absolute',
+    height: '100%',
   },
 }))
 
@@ -73,9 +110,7 @@ const Sponsors = () => {
   return (
     <Box className={classes.root}>
       <Box className={classes.sectionTitle}>
-        <Typography variant="h3" className={classes.header}>
-          OUR SPONSORS
-        </Typography>
+        <Typography variant="h3">OUR SPONSORS</Typography>
       </Box>
       <Box className={classes.description}>
         <Typography>
@@ -89,7 +124,47 @@ const Sponsors = () => {
         </Typography>
       </Box>
       <Box className={classes.sponsorLogoWrapper}>
-        <Button variant="contained"></Button>
+        <button className={classes.bybitLogoButton}>
+          <img
+            src="https://res.cloudinary.com/jianoway/image/upload/v1638360412/bybit-logo_svetqo.png"
+            alt="ByBit Logo"
+            onClick={() => window.open('https://www.bybit.com/en-US/', 'ByBit')}
+          />
+        </button>
+        <button className={classes.rippleLogoButton}>
+          <img
+            src="https://res.cloudinary.com/jianoway/image/upload/v1638360411/ripple-logo_zmlfxh.png"
+            alt="Ripple"
+            onClick={() => window.open('https://ripple.com/', 'Ripple')}
+          />
+        </button>
+        <button className={classes.stellarLogoButton} position="right">
+          <img
+            src="https://res.cloudinary.com/jianoway/image/upload/v1638360412/stellar-logo_dnsjor.png"
+            alt="Stellar Logo"
+            onClick={() => window.open('https://www.stellar.org/', 'Stellar')}
+            height="100%"
+            width="240px"
+          />
+        </button>
+      </Box>
+      <Box className={classes.partnersTitle}>
+        <Typography variant="h3">Schools and University Partners:</Typography>
+      </Box>
+      <Box className={classes.partnersLogoWrapper}>
+        <button className={classes.nusLogoButton}>
+          <img
+            src="https://res.cloudinary.com/jianoway/image/upload/c_scale,w_362/v1638365359/nus-logo_vxbv5m.png"
+            alt="Stellar Logo"
+            onClick={() => window.open('https://www.nus.edu.sg/', 'NUS')}
+          />
+        </button>
+        <Box className={classes.ftsLogo}>
+          <img
+            src="https://res.cloudinary.com/jianoway/image/upload/c_scale,w_360/v1638365359/fts-logo_ccxzfz.png"
+            alt="NUS FTS"
+          />
+        </Box>
       </Box>
     </Box>
   )

ff7a70aa63981af179013e36c35a01c8327deb9e
Zhang Zhikai <zhangzhikai71@gmail.com>
2021-12-08 05:36:02 +0800

    Editted Imports

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Box, Typography } from '@material-ui/core'
+import { Box, Typography, IconButton, Menu } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
-import { Menu } from '@material-ui/core'
-import { IconButton } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 

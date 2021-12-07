e1e8368f2d81bc8da5098a7d97d2eab7bdf18973
Ivander <ivanderjmw@gmail.com>
2021-12-08 02:03:40 +0800

    remove nonexistent import

--- src/views/Timeline/index.jsx
+++ src/views/Timeline/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { Box, Typography } from '@material-ui/core'
-import CompetitionTimeline from '../../components/CompetitionTimeline'
 import MyTimeline from '../../components/MyTimeline'
 
 const useStyles = makeStyles(() => ({

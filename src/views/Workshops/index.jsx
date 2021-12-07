50485ec5198820655f87f02e1ad5bff94b98228a
jianoway <77208762+jianoway@users.noreply.github.com>
2021-12-08 00:44:54 +0800

    Add heading to workshops

--- src/views/Workshops/index.jsx
+++ src/views/Workshops/index.jsx
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Box, Typography } from '@material-ui/core'
 import WorkshopRenderer from './WorkshopRenderer'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
@@ -16,9 +16,25 @@ const useStyles = makeStyles(() => ({
     fontWeight: 700,
     margin: 'auto',
     width: '100%',
+    maxWidth: '960px',
+    paddingBottom: '35px',
+    paddingTop: '60px',
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+      justifyContent: 'center',
+    },
   },
   overview: {
-    textAlign: 'left',
+    textAlign: 'justify',
+    margin: 'auto',
+    width: '100%',
+    maxWidth: '960px',
+    paddingBottom: '45px',
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'left',
+      maxWidth: '460px',
+      justifyContent: 'center',
+    },
   },
 }))
 
@@ -27,10 +43,34 @@ const Workshops = () => {
   const renderedWorkshops = WorkshopRenderer()
   return (
     <Layout>
-      <Typography className={classes.title} variant="h5" textAlign="left">
-        General Workshop Details:
-      </Typography>
-      <Box className={classes.root}>{renderedWorkshops}</Box>
+      <Box
+        display="flex"
+        textAlign="left"
+        margin="auto"
+        width="100%"
+        flexDirection="row"
+      >
+        <Box width="30%"></Box>
+      </Box>
+      <Box className={classes.root}>
+        <Typography className={classes.title} variant="h5">
+          General Workshop Details:
+        </Typography>
+        <Typography className={classes.overview}>
+          <ul>
+            <li>
+              Participants will earn a verifiable certificate of participation
+              when participating in each workshop.
+            </li>
+            <li>
+              Attendace will be taken during the workshops. A full attendance is
+              required in order to receive the certificates.
+            </li>
+          </ul>
+        </Typography>
+
+        {renderedWorkshops}
+      </Box>
     </Layout>
   )
 }

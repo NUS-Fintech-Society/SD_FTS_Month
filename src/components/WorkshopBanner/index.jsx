4859f97b51f077d90a58684a4fca2f974c6773f2
jerrenss <gerrenseow@gmail.com>
2021-12-08 22:02:40 +0800

    refactor css

--- src/components/WorkshopBanner/index.jsx
+++ src/components/WorkshopBanner/index.jsx
@@ -1,42 +1,25 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { Box, Button, Container, Typography, Grid } from '@material-ui/core'
+import DATA from '../../data/speakers'
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    padding: '54px 16px',
+    backgroundImage: `url(${'WorkshopBanner.svg'})`,
+    backgroundSize: 'cover',
+  },
+
+  contentWrapper: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: theme.palette.background.secondary,
-    padding: '0px 16px',
-    position: 'relative',
-    marginBottom: 90,
-  },
-
-  SVG: {
-    position: 'absolute',
-    left: '50%',
-    padding: 'None',
-    minWidth: '100%',
-    minHeight: '30%',
-    top: '100%',
-    transform: 'translate(-50%, -95%)',
-    zIndex: 0,
-    [theme.breakpoints.down('sm')]: {
-      height: '105.5%',
-      width: 'auto',
-    },
-  },
-
-  contentWrapper: {
     textAlign: 'center',
   },
 
   header: {
-    textAlign: 'center',
     marginBottom: 40,
-    position: 'relative',
     color: theme.palette.text.contrast,
   },
 
@@ -46,19 +29,13 @@ const useStyles = makeStyles((theme) => ({
 
   text: {
     color: theme.palette.text.contrast,
-    position: 'relative',
-    textAlign: 'center',
   },
 
   textHighlight: {
-    position: 'relative',
-    textAlign: 'center',
     color: '#EC255A',
   },
 
   buttonWrapper: {
-    marginTop: 50,
-    marginBottom: '100px',
     borderRadius: '50px',
     height: '80px',
     width: '320px',
@@ -69,10 +46,6 @@ const useStyles = makeStyles((theme) => ({
         fontSize: '1em',
       },
     },
-
-    '& :not(:first-child)': {
-      marginLeft: 12,
-    },
   },
 
   buttontext: {
@@ -81,32 +54,53 @@ const useStyles = makeStyles((theme) => ({
   },
 
   image: {
-    alignself: 'center',
     maxWidth: '100%',
-    height: 'auto',
-    position: 'relative',
   },
 
   speakerText: {
     color: theme.palette.text.contrast,
-    position: 'relative',
-    textAlign: 'center',
   },
 
   gridBox: {
     padding: 40,
-    margin: 20,
+    [theme.breakpoints.down('sm')]: {
+      padding: 16,
+    },
   },
 }))
 
 export default function WorkshopBanner() {
   const classes = useStyles()
 
+  const renderSpeakers = () => {
+    return DATA.map((speaker, index) => {
+      return (
+        <Grid item xs={6} md={3} key={index}>
+          <Box className={classes.gridBox}>
+            <img
+              className={classes.image}
+              src="speaker1.png"
+              alt={speaker.name}
+            />
+            <Typography className={classes.speakerText}>
+              {speaker.name}
+            </Typography>
+            <Typography className={classes.speakerText}>
+              {speaker.role}
+            </Typography>
+            <Typography className={classes.speakerText}>
+              {speaker.company}
+            </Typography>
+          </Box>
+        </Grid>
+      )
+    })
+  }
+
   return (
     <Box className={classes.root}>
       <Container className={classes.contentWrapper} maxWidth="md">
-        <img src="WorkshopBanner.svg" className={classes.SVG} />
-        <Typography variant="h6" className={classes.header}>
+        <Typography variant="h3" className={classes.header}>
           WORKSHOPS
         </Typography>
         <Box className={classes.textWrapper}>
@@ -122,134 +116,7 @@ export default function WorkshopBanner() {
             *Attendance would be taken during the workshops
           </Typography>
 
-          <div>
-            <Grid
-              container
-              direction="row"
-              justify="center"
-              alignItems="center"
-            >
-              <Grid item xs={6} md={3}>
-                <Box className={classes.gridBox}>
-                  <img className={classes.image} src="speaker1.png" alt="" />
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lorem Ipsum{' '}
-                  </Typography>
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lead Data Scientist, Analytics
-                  </Typography>
-                  <Typography className={classes.speakerText}> Grab</Typography>
-                </Box>
-              </Grid>
-
-              <Grid item xs={6} md={3}>
-                <Box className={classes.gridBox}>
-                  <img className={classes.image} src="speaker1.png" alt="" />
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lorem Ipsum{' '}
-                  </Typography>
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lead Data Scientist, Analytics
-                  </Typography>
-                  <Typography className={classes.speakerText}> Grab</Typography>
-                </Box>
-              </Grid>
-
-              <Grid item xs={6} md={3}>
-                <Box className={classes.gridBox}>
-                  <img className={classes.image} src="speaker1.png" alt="" />
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lorem Ipsum{' '}
-                  </Typography>
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lead Data Scientist, Analytics
-                  </Typography>
-                  <Typography className={classes.speakerText}> Grab</Typography>
-                </Box>
-              </Grid>
-
-              <Grid item xs={6} md={3}>
-                <Box className={classes.gridBox}>
-                  <img className={classes.image} src="speaker1.png" alt="" />
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lorem Ipsum{' '}
-                  </Typography>
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lead Data Scientist, Analytics
-                  </Typography>
-                  <Typography className={classes.speakerText}> Grab</Typography>
-                </Box>
-              </Grid>
-
-              <Grid item xs={6} md={3}>
-                <Box className={classes.gridBox}>
-                  <img className={classes.image} src="speaker1.png" alt="" />
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lorem Ipsum{' '}
-                  </Typography>
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lead Data Scientist, Analytics
-                  </Typography>
-                  <Typography className={classes.speakerText}> Grab</Typography>
-                </Box>
-              </Grid>
-
-              <Grid item xs={6} md={3}>
-                <Box className={classes.gridBox}>
-                  <img className={classes.image} src="speaker1.png" alt="" />
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lorem Ipsum{' '}
-                  </Typography>
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lead Data Scientist, Analytics
-                  </Typography>
-                  <Typography className={classes.speakerText}> Grab</Typography>
-                </Box>
-              </Grid>
-
-              <Grid item xs={6} md={3}>
-                <Box className={classes.gridBox}>
-                  <img className={classes.image} src="speaker1.png" alt="" />
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lorem Ipsum{' '}
-                  </Typography>
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lead Data Scientist, Analytics
-                  </Typography>
-                  <Typography className={classes.speakerText}> Grab</Typography>
-                </Box>
-              </Grid>
-
-              <Grid item xs={6} md={3}>
-                <Box className={classes.gridBox}>
-                  <img className={classes.image} src="speaker1.png" alt="" />
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lorem Ipsum{' '}
-                  </Typography>
-                  <Typography className={classes.speakerText}>
-                    {' '}
-                    Lead Data Scientist, Analytics
-                  </Typography>
-                  <Typography className={classes.speakerText}> Grab</Typography>
-                </Box>
-              </Grid>
-            </Grid>
-          </div>
+          <Grid container>{renderSpeakers()}</Grid>
 
           <Button
             color="primary"

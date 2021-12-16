fe40b308d9ad3d7afbfe30dc719835f4b141c03f
jerrenss <gerrenseow@gmail.com>
2021-12-16 16:14:44 +0800

    incorporate google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,55 +1,89 @@
-import React from 'react'
-import App from 'next/app'
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+
 import { StylesProvider } from '@material-ui/styles'
 import { ThemeProvider as StyledComponentProvider } from 'styled-components'
 import { ThemeProvider as MaterialUiProvider } from '@material-ui/core/styles'
-
 import CssBaseline from '@material-ui/core/CssBaseline'
-
 import theme from '../themes/'
 
-export default class MyApp extends App {
-  componentDidMount() {
+import * as gtag from '../lib/gtag'
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props
+  const router = useRouter()
+
+  useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles)
     }
-  }
+  }, [])
 
-  render() {
-    const { Component, pageProps } = this.props
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
-    return (
-      <>
-        <Head>
-          <title>NUS Fintech Month</title>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="favicon.png" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <StyledComponentProvider theme={theme}>
-          <MaterialUiProvider theme={theme}>
-            <StylesProvider injectFirst>
-              <CssBaseline />
-              <Component {...pageProps} />
-            </StylesProvider>
-          </MaterialUiProvider>
-        </StyledComponentProvider>
-      </>
-    )
-  }
+  return (
+    <>
+      <Head>
+        <title>NUS Fintech Month</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <link rel="shortcut icon" href="favicon.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+        <script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <StyledComponentProvider theme={theme}>
+        <MaterialUiProvider theme={theme}>
+          <StylesProvider injectFirst>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </StylesProvider>
+        </MaterialUiProvider>
+      </StyledComponentProvider>
+    </>
+  )
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any,
 }

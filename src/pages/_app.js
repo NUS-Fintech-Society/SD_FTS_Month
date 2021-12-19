5f3fd4bc1a09e94e3dee490dd640197751c90e00
jerrenss <gerrenseow@gmail.com>
2021-12-19 15:55:16 +0800

    update meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -36,6 +36,13 @@ export default function MyApp(props) {
     <>
       <Head>
         <title>NUS Fintech Month</title>
+        <meta description="Official website for the annual Fintech Month, proudly hosted by NUS Fintech Society" />
+
+        <meta property="og:title" content="NUS Fintech Month" />
+        <meta
+          property="og:description"
+          content="Official website for the annual Fintech Month, proudly hosted by NUS Fintech Society"
+        />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"

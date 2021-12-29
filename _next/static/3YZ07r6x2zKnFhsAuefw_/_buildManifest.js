15f4824431cb62dbe7b2bc4668f0840eefb87469
jerrenss <50544486+jerrenss@users.noreply.github.com>
2021-12-29 18:10:15 +0000

    Deploying to gh-pages from @ NUS-Fintech-Society/SD_FTS_Month@194d1014ce6877229fc196082a5a19904f9f06ff 🚀

---
+++ _next/static/3YZ07r6x2zKnFhsAuefw_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-ccd10f3c9aa1a3f86b09.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9753916cb6ae42283fac.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

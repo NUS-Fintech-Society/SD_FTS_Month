8f7ae06b8ea55578d4151a04a76c9d16857db9bc
jerrenss <gerrenseow@gmail.com>
2021-12-30 21:54:18 +0800

    update credits board

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Box, Typography } from '@material-ui/core'
+import { Box, Grid, Typography } from '@material-ui/core'
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -32,163 +32,13 @@ const useStyles = makeStyles((theme) => ({
       fontSize: 20,
     },
   },
-  bybitImageWrapper: {
-    cursor: 'pointer',
-    height: 120,
-    width: 480,
-    [theme.breakpoints.down('xs')]: {
-      height: 70,
-      width: 250,
-    },
-  },
   rowWrapper: {
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'center',
     marginTop: 32,
-  },
-  rippleImageWrapper: {
-    cursor: 'pointer',
-    height: 120,
-    width: 250,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 65,
-      width: 120,
-    },
-  },
-  stellarImageWrapper: {
-    cursor: 'pointer',
-    height: 120,
-    width: 300,
-    [theme.breakpoints.down('xs')]: {
-      height: 70,
-      width: 170,
-    },
-  },
-  ftsImageWrapper: {
-    cursor: 'pointer',
-    height: 120,
-    width: 200,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 75,
-      width: 120,
-    },
-  },
-  ftlImageWrapper: {
-    cursor: 'pointer',
-    height: 110,
-    width: 350,
-    [theme.breakpoints.down('xs')]: {
-      height: 55,
-      width: 170,
-    },
-  },
-  alcImageWrapper: {
-    cursor: 'pointer',
-    height: 80,
-    width: 150,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 40,
-      width: 80,
-    },
-  },
-  dfcImageWrapper: {
-    cursor: 'pointer',
-    height: 80,
-    width: 200,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 30,
-      width: 80,
-    },
-  },
-  futuImageWrapper: {
-    cursor: 'pointer',
-    height: 80,
-    width: 150,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 40,
-      width: 80,
-    },
-  },
-  hlcImageWrapper: {
-    cursor: 'pointer',
-    height: 50,
-    width: 200,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 25,
-      width: 80,
-    },
-  },
-  cpiImageWrapper: {
-    cursor: 'pointer',
-    height: 50,
-    width: 200,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 20,
-      width: 80,
-    },
-  },
-  msrImageWrapper: {
-    cursor: 'pointer',
-    height: 35,
-    width: 220,
-    [theme.breakpoints.down('xs')]: {
-      height: 15,
-      width: 80,
-    },
-  },
-  otvImageWrapper: {
-    cursor: 'pointer',
-    height: 80,
-    width: 110,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 40,
-      width: 70,
-    },
-  },
-  ptmImageWrapper: {
-    cursor: 'pointer',
-    height: 100,
-    width: 100,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 50,
-      width: 50,
-    },
-  },
-  spmImageWrapper: {
-    cursor: 'pointer',
-    height: 50,
-    width: 220,
-    [theme.breakpoints.down('xs')]: {
-      height: 20,
-      width: 80,
-    },
-  },
-  stwImageWrapper: {
-    cursor: 'pointer',
-    height: 50,
-    width: 220,
-    marginRight: 24,
-    [theme.breakpoints.down('xs')]: {
-      height: 25,
-      width: 100,
-    },
-  },
-  shpImageWrapper: {
-    cursor: 'pointer',
-    height: 70,
-    width: 220,
-    [theme.breakpoints.down('xs')]: {
-      height: 30,
-      width: 80,
+    '& .MuiGrid-item': {
+      cursor: 'pointer',
     },
   },
 }))
@@ -205,188 +55,198 @@ const Sponsors = () => {
         <Typography variant="h5" className={classes.sectionHeader}>
           Sponsors
         </Typography>
-        <Box className={classes.rowWrapper}>
-          <Box className={classes.bybitImageWrapper}>
+        <Grid
+          container
+          className={classes.rowWrapper}
+          spacing={4}
+          style={{ maxWidth: '600px' }}
+        >
+          <Grid item xs={12}>
             <img
               src="/credits/bybit-logo-trans.png"
               alt="Bybit"
-              height="100%"
               width="100%"
               onClick={() =>
                 window.open('https://www.bybit.com/en-US/', '_blank')
               }
             />
-          </Box>
-        </Box>
-        <Box className={classes.rowWrapper}>
-          <div className={classes.rippleImageWrapper}>
+          </Grid>
+          <Grid item xs={6}>
             <img
               src="/credits/ripple-logo.png"
               alt="Ripple"
-              height="100%"
               width="100%"
               onClick={() => window.open('https://ripple.com/', '_blank')}
             />
-          </div>
-          <div className={classes.stellarImageWrapper}>
+          </Grid>
+          <Grid item xs={6}>
             <img
               src="/credits/stellar-logo.png"
               alt="Stellar"
               onClick={() => window.open('https://www.stellar.org/', '_blank')}
-              height="100%"
               width="100%"
             />
-          </div>
-        </Box>
+          </Grid>
+        </Grid>
       </Box>
       <Box className={classes.sectionWrapper}>
         <Typography variant="h5" className={classes.sectionHeader}>
           Organisers
         </Typography>
-        <Box className={classes.rowWrapper}>
-          <div className={classes.ftsImageWrapper}>
+        <Grid
+          container
+          className={classes.rowWrapper}
+          spacing={4}
+          style={{ maxWidth: '600px' }}
+        >
+          <Grid item xs={6}>
             <img
               src="/credits/SocietyLogo.png"
               alt="NUS FTS"
-              height="100%"
               width="100%"
               onClick={() =>
                 window.open('https://fintechsociety.comp.nus.edu.sg/', '_blank')
               }
             />
-          </div>
-          <div className={classes.ftlImageWrapper}>
+          </Grid>
+          <Grid item xs={6}>
             <img
               src="/credits/nus_ftl_logo.png"
               alt="NUS FTL"
-              height="100%"
               width="100%"
               onClick={() =>
                 window.open('https://fintechlab.nus.edu.sg/', '_blank')
               }
             />
-          </div>
-        </Box>
+          </Grid>
+        </Grid>
       </Box>
       <Box className={classes.sectionWrapper}>
         <Typography variant="h5" className={classes.sectionHeader}>
           Partners
         </Typography>
-        <Box className={classes.rowWrapper}>
-          <div className={classes.alcImageWrapper}>
+        <Grid
+          container
+          className={classes.rowWrapper}
+          spacing={4}
+          style={{ maxWidth: '900px' }}
+        >
+          <Grid item xs={6} md={3}>
             <img
               src="/credits/alphalab-logo.png"
               alt="AlphaLab Capital"
-              height="100%"
               width="100%"
               onClick={() =>
                 window.open('https://www.alphalab.capital/', '_blank')
               }
             />
-          </div>
-          <div className={classes.dfcImageWrapper}>
+          </Grid>
+          <Grid item xs={6} md={3}>
             <img
               src="/credits/defiance-capital-logo.png"
               alt="DeFiance Capital"
-              height="100%"
               width="100%"
               onClick={() =>
                 window.open('https://www.defiance.capital/', '_blank')
               }
             />
-          </div>
-          <div className={classes.futuImageWrapper}>
+          </Grid>
+          <Grid item xs={6} md={3}>
             <img
               src="/credits/futu-singapore-logo.png"
               alt="Futu Singapore"
-              height="100%"
               width="100%"
               onClick={() => window.open('https://www.futusg.com/', '_blank')}
             />
-          </div>
-        </Box>
-        <Box className={classes.rowWrapper}>
-          <div className={classes.hlcImageWrapper}>
+          </Grid>
+          <Grid item xs={6} md={3}>
             <img
               src="/credits/helicap-logo.png"
               alt="Helicap"
-              height="100%"
               width="100%"
               onClick={() => window.open('https://heli-cap.com/', '_blank')}
             />
-          </div>
-          <div className={classes.cpiImageWrapper}>
-            <img
-              src="/credits/index-coop.png"
-              alt="Index Coop"
-              height="100%"
-              width="100%"
-              onClick={() =>
-                window.open('https://www.indexcoop.com/', '_blank')
-              }
-            />
-          </div>
-          <div className={classes.msrImageWrapper}>
+          </Grid>
+          <Grid item xs={6} md={3}>
             <img
               src="/credits/messari-logo.png"
               alt="Messari"
-              height="100%"
               width="100%"
               onClick={() => window.open('https://messari.io/', '_blank')}
             />
-          </div>
-        </Box>
-        <Box className={classes.rowWrapper}>
-          <div className={classes.otvImageWrapper}>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            md={3}
+            style={{ paddingLeft: '48px', paddingRight: '48px' }}
+          >
             <img
               src="/credits/octava-logo.png"
               alt="Octava"
-              height="100%"
               width="100%"
               onClick={() => window.open('https://www.octava.sg/', '_blank')}
             />
-          </div>
-          <div className={classes.ptmImageWrapper}>
+          </Grid>
+          <Grid item xs={6} md={3}>
             <img
-              src="/credits/platus-mazu-logo.png"
-              alt="Platus Mazu"
-              height="100%"
+              src="/credits/revolut-logo.png"
+              alt="Revolut"
               width="100%"
               onClick={() =>
-                window.open('https://www.plutusmazu.com/', '_blank')
+                window.open('https://www.revolut.com/en-SG', '_blank')
               }
             />
-          </div>
-          <div className={classes.spmImageWrapper}>
+          </Grid>
+          <Grid item xs={6} md={3}>
             <img
               src="/credits/spenmo-logo.png"
               alt="Spenmo"
-              height="100%"
               width="100%"
               onClick={() => window.open('https://spenmo.com/', '_blank')}
             />
-          </div>
-        </Box>
-        <Box className={classes.rowWrapper}>
-          <div className={classes.stwImageWrapper}>
+          </Grid>
+          <Grid item xs={6} md={3}>
             <img
               src="/credits/stashaway-logo.png"
               alt="Stashaway"
-              height="100%"
               width="100%"
               onClick={() => window.open('https://www.stashaway.sg/', '_blank')}
             />
-          </div>
-          <div className={classes.shpImageWrapper}>
+          </Grid>
+          <Grid item xs={6} md={3}>
             <img
               src="/credits/shopee_logo.png"
               alt="Shopee"
-              height="100%"
               width="100%"
               onClick={() => window.open('https://shopee.sg/', '_blank')}
             />
-          </div>
-        </Box>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <img
+              src="/credits/oio-logo.png"
+              alt="OIO"
+              width="100%"
+              onClick={() => window.open('https://oio.holdings/', '_blank')}
+            />
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <img
+              src="/credits/startbahn-logo.png"
+              alt="Startbahn"
+              width="100%"
+              onClick={() => window.open('https://startbahn.jp/en/', '_blank')}
+            />
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <img
+              src="/credits/hg-exchange-logo.webp"
+              alt="HG Exchange"
+              width="100%"
+              onClick={() => window.open('https://hg.exchange/', '_blank')}
+            />
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   )
